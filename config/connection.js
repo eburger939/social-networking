f1eaf11ddf1c0bf16b532ad2ddc6f5f336d7e5bf
Emily Burger <eburger939@gmail.com>
2022-02-07 18:48:23 -0600

    user model complete

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-// Wrap Mongoose around local connection to MongoDB
+
 mongoose.connect('mongodb://localhost:27017/social_networkDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -8,5 +8,5 @@ mongoose.connect('mongodb://localhost:27017/social_networkDB', {
   useFindAndModify: false,
 });
 
-// Export connection 
+
 module.exports = mongoose.connection;
---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose')
+
+const validateEmail = function(email) {
+    const emailT = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    return emailT.test(email) 
+}
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'Username is required',
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: 'Email address is required',
+            unique: true,
+            valdidate: [validateEmail, 'Please add a valid email address'],
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please add a valid email address']
+
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema
+.virtual('friendCount')
+.get(function () {
+    return `This user has ${this.friends.length} friends`;
+})
+
+const User = model('user', userSchema)
+
+module.exports = User;
